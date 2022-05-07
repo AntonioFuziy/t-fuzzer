e274991b753ff3fec5edd3cd68f0b2baccad37a7
AntonioFuziy <antoniovf@al.insper.edu.br>
2022-05-07 18:42:40 -0300

    feat: added js script and popup extension

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-document.body.style.border = "5px solid red";
+const checkCookies = () => {
+  let allCookies = document.cookie.split(';');
+  let cookies = "";
+  for (let i = 0; i < allCookies.length; i++) {
+    cookies += allCookies[i] + "\n";
+  }
+  return cookies;
+}
+let allCookies = checkCookies()
+console.log("Cookies: ", allCookies)
+document.getElementById("cookies").innerHTML = allCookies;
